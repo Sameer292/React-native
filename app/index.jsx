232e9534c2aa8da0eeb96e45577e3ba4692aabb8
Sameer paudel <paudelsameer888@gmail.com>
2025-02-05 23:36:14 +0545

    Add day one stuffs

---
+++ app/index.jsx
@@ -0,0 +1,72 @@
+import { View, Text, StyleSheet, ImageBackground, Pressable } from 'react-native'
+// import React from 'react'
+import { Link } from 'expo-router'
+import icedCoffee from '@/assets/images/iced-coffee.png'
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={icedCoffee}
+        resizeMode='cover'
+        style={styles.image}
+      >
+        <Text style={styles.text}>mero coffee shop</Text>
+
+        <Link href="/contact" style={{ marginHorizontal: 'auto' }} asChild >
+          <Pressable style={styles.buttonstyle}>
+            <Text style={styles.buttonText}>
+              Contact Us
+            </Text>
+          </Pressable>
+        </Link>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default app
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    marginBottom: 120
+  },
+  link: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    padding: 4,
+  },
+  buttonstyle: {
+    height: 60,
+    borderRadius: 20,
+    backgroundColor: 'rgba(0,0,0,0.75)',
+    justifyContent: 'center',
+    padding: 6
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 4,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  }
+})
