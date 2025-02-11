5c9a56a68bb931e4c0729d7c8fabe3e7e4b5532a
Sameer paudel <paudelsameer888@gmail.com>
2025-02-11 23:10:42 +0545

    Components created

--- app/(tabs)/contact.jsx
+++ app/(tabs)/contact.jsx
@@ -1,51 +1,42 @@
-import { Link } from 'expo-router'
-import React, { Component, useState } from 'react'
-import { FlatList, TouchableOpacity } from 'react-native'
-import { Text, View, StyleSheet, Button, Pressable, Alert, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import { FlatList,Text, View, StyleSheet, Alert } from 'react-native'
+import Goalitem from '@/components/Goalitem'
+import Goalinput from '@/components/Goalinput'
 
 function contact() {
 
-  const [enteredName, setenteredName] = useState('');
   const [listOfCoffee, setlistOfCoffee] = useState([]);
 
 
-  function listInputhandler(text) {
-    setenteredName(text);
-  }
 
-  function addListhandler() {
+  function addListhandler(enteredName) {
 
     if (enteredName.length === 0) {
       Alert.alert('Please enter the name of the coffee please');
       return;
     }
     //Just use key instead. it's much easier setlistOfCoffee([...listOfCoffee, { text:enteredName, key:Math.random().toString() }]);
-    setlistOfCoffee([...listOfCoffee, { text:enteredName, id:Math.random().toString() }]);
+    setlistOfCoffee([...listOfCoffee, {text:enteredName, id:Math.random().toString()} ]);
     //The id is put for example
     // setlistOfCoffee([])
-    setenteredName('')
+    
 
   }
 
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput defaultValue={enteredName} onChangeText={listInputhandler} style={styles.textinp} placeholder='Enter name of the coffee' />
-        <TouchableOpacity onPress={addListhandler} style={styles.buttonEnter}>
-          <Text style={styles.entertext}>Enter</Text>
-        </TouchableOpacity>
-      </View>
+      <Goalinput  addListhandler={addListhandler} />
       <View style={styles.Listcontainer}>
         <Text>
           Your list of coffee are...
         </Text>
 
         <FlatList data={listOfCoffee} renderItem={
-          (itemData) => <Text style={styles.coffee}>{itemData.item.text}</Text>
-          }
-          keyExtractor={(item,index) => item.id}
-           />
-           {/* The keyExtractor is used to identify the unique key of the item use key in the data object instead, makes it easier  */}
+          (itemData) => <Goalitem coffee={itemData.item.text} />
+        }
+          keyExtractor={(item, index) => item.id}
+        />
+        {/* The keyExtractor is used to identify the unique key of the item use key in the data object instead, makes it easier  */}
 
       </View>
     </View>
@@ -56,12 +47,6 @@ function contact() {
 export default contact
 
 const styles = StyleSheet.create({
-  entertext: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
   container: {
     flex: 1,
     flexDirection: 'column',
@@ -74,51 +59,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  textinp: {
-    borderColor: 'black',
-    borderWidth: 1,
-    color: '#03122f',
-    fontSize: 16,
-    fontWeight: 'bold',
-    paddingLeft: 20,
-    width: '70%',
-    height: 60,
-    borderRadius: 8,
-    backgroundColor: '#F3DADF',
-  },
-  buttonEnter: {
-    height: 60,
-    borderRadius: 10,
-    backgroundColor: '#274D60',
-    justifyContent: 'center',
-    width: '25%',
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginTop: 50,
-    paddingBottom: 20,
-    marginBottom: 20,
-    borderBottomColor: 'black',
-    borderBottomWidth: 1
-  },
   Listcontainer: {
-    flex: 6,
+    flex: 8,
     flexDirection: 'column',
     justifyContent: 'flex-start',
-  },
-  coffee: {
-    color: '#f3dadf',
-    // color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-    margin: 6,
-    paddingVertical: 10,
-    borderRadius: 8,
-    backgroundColor: '#413B61',
-    paddingLeft: 20,
   }
 
 
